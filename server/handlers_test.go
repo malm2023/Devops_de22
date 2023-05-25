46ad3b2e70b10d75d11321049f0f96802ec3c517
malm2023 <almemark@mail.com>
2023-05-25 13:08:59 +0200

    Change greeting message in handlers.go and handlers_test.go

--- server/handlers_test.go
+++ server/handlers_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestHelloHandler(t *testing.T) {
 	wantCode := http.StatusOK
-	wantBody := "Hejsan, nackademin & de22"
+	wantBody := "Hej, nackademin & de22"
 
 	srv := &server{
 		httpServer: &http.Server{
